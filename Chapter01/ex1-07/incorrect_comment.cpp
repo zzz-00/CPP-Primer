b3b354cdc4e7a0ab39c902ae571659f27305ae7c
zzz-00 <576423404@qq.com>
2023-10-16 16:48:23 +0800

    add ex1-07

---
+++ Chapter01/ex1-07/incorrect_comment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/*
+ * This is a program that has incorrectly nested comments.
+ *
+ * We will get the error below.
+ * error: expected unqualified-id before '/' token
+ */
+int main()
+{
+    return 0;
+}
