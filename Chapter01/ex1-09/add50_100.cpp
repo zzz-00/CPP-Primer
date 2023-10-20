4668292228cd3b3b545db97817c3cac35d83da8b
zzz-00 <576423404@qq.com>
2023-10-20 16:26:18 +0800

    add ex1-09

---
+++ Chapter01/ex1-09/add50_100.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    int sum = 0;
+    int val = 50;
+    while (val <= 100)
+    {
+        sum += val;
+        ++val;
+    }
+    std::cout << "sum = " << sum << std::endl;
+    return 0;
+}
