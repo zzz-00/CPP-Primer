da9bdf98ed92397e605f74befe6682aef9a39721
zzz-00 <576423404@qq.com>
2023-11-01 17:54:55 +0800

    modify

--- Chapter01/ex1-11/print_number.cpp
+++ Chapter01/ex1-11/print_number.cpp
@@ -2,11 +2,24 @@
 int main()
 {
     int start = 0, end = 0;
+    std::cout << "Please enter two numbers:" << std::endl;
     std::cin >> start >> end;
     std::cout << std::endl;
-    while (start <= end)
+    if (start < end)
     {
-        std::cout << start << std::endl;
-        start++;
+        while (start <= end)
+        {
+            std::cout << start << std::endl;
+            start++;
+        }
     }
+    else
+    {
+        while (start >= end)
+        {
+            std::cout << start << std::endl;
+            start--;
+        }
+    }
+    return 0;
 }
