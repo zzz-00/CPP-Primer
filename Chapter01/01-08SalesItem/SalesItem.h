6fd0e6604635dc470e25fd6302a3c458c356f00f
zzz-00 <576423404@qq.com>
2023-11-02 12:19:17 +0800

    modify

--- Chapter01/01-08SalesItem/SalesItem.h
+++ Chapter01/01-08SalesItem/SalesItem.h
@@ -13,8 +13,7 @@ class Sales_item
     friend std::istream &operator>>(std::istream &, Sales_item &);
     friend std::ostream &operator<<(std::ostream &, const Sales_item &);
     friend bool operator<(const Sales_item &, const Sales_item &);
-    friend bool
-    operator==(const Sales_item &, const Sales_item &);
+    friend bool operator==(const Sales_item &, const Sales_item &);
 
 public:
     // constructors are explained in section 7.1.4, pages 262 - 265
