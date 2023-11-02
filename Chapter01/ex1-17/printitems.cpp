f6ee696f46d22f09e4186e3aa96ce470bae3ad99
zzz-00 <576423404@qq.com>
2023-11-02 13:13:37 +0800

    add ex1-20

---
+++ Chapter01/ex1-17/printitems.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "salesitem.h"
+int main()
+{
+    Sales_item book;
+    std::cout << "请输入销售记录" << std::endl;
+    while (std::cin >> book)
+    {
+        std::cout << "ISBN 售出本数 销售额为：" << std::endl;
+        std::cout << book << std::endl;
+    }
+    return 0;
+}
