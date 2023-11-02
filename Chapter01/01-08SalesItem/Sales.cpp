c01731dcbc993d9cc1b72acac22156024e0f5c9c
zzz-00 <576423404@qq.com>
2023-11-02 11:49:57 +0800

    add sales_item

---
+++ Chapter01/01-08SalesItem/Sales.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "SalesItem.h"
+
+int main()
+{
+    Sales_item book;
+    // read ISBN, number of copies sold, and sales price
+    std::cin >> book;
+    // write ISBN, number of copies sold, total revenue, and average price
+    std::cout << book << std::endl;
+    return 0;
+}
