40ba568f13e54267f4486cd740d1146b51654b56
zzz-00 <576423404@qq.com>
2023-11-02 14:01:27 +0800

    add 01-10checkisbn

--- Chapter01/01-10checkisbn/checkisbn.cpp
+++ Chapter01/01-10checkisbn/checkisbn.cpp
@@ -2,4 +2,16 @@
 #include "salesitem.h"
 int main()
 {
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    if (item1.isbn() == item2.isbn())
+    {
+        std::cout << item1 + item2 << std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cerr << "Data must refer to same ISBN" << std::endl;
+        return -1;
+    }
 }
