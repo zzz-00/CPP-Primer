31721a8b589ec96141938f628b1d25ce0de1d1ae
zzz-00 <576423404@qq.com>
2023-11-02 13:51:23 +0800

    add ex1-22

---
+++ Chapter01/ex1-22/printitemsum2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "salesitem.h"
+
+int main()
+{
+    Sales_item book, total;
+    if (std::cin >> total)
+    {
+        while (std::cin >> book)
+        {
+            if (compareIsbn(total, book))
+            {
+                total += book;
+            }
+            else
+            {
+                std::cout << "输入了不同的ISBN" << std::endl;
+                return -1;
+            }
+        }
+        std::cout << total << std::endl;
+    }
+    else
+    {
+        std::cout << "没有数据" << std::endl;
+        return -1;
+    }
+    return 0;
+}
